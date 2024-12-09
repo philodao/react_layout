48a4d7c961acc9325ed20c0f13ada9b3e05f4828
philodao <51352265+philodao@users.noreply.github.com>
2024-12-09 18:50:35 +0900

    typescript 변환 오류 수정

--- src/component/BrandLayout.tsx
+++ src/component/BrandLayout.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { List } from "@phosphor-icons/react";
 import '../_assets/css/reset.css'
 import '../_assets/css/common.css'
 import '../_assets/css/brand.css'
@@ -10,12 +11,22 @@ export interface BrandLayoutProps {
 export function BrandLayout(props: BrandLayoutProps){
     return (
         <>
-            <div className="wrapper">
-                <div className="sidebar-wrapper">
-                    <div className="simplebar-wrapper">
+            <div className='wrapper'>
+                <div className='sidebar-wrapper'>
+                    <div className='simplebar-wrapper'>
                         
                     </div>
                 </div>
+
+                <header>
+                    <div className='topbar'>
+                        <div className='navbar divFlex align-items-center'>
+                            <div className='mobile-toggle-menu'>
+                                <List size={32} />
+                            </div>
+                        </div>
+                    </div>
+                </header>
 
                 <div className="brandSite">
                 {props.children}
